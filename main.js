5a57d44aaa5af7e3320758280147f149c97bedd8
NathanRomike <nathanromike@gmail.com>
2016-08-27 17:14:41 -0700

    Add drop and jump animations.

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ var mainState = {
 
     game.load.image('robot', 'assets/robot.png');
 
+    game.load.image('robot', 'assets/robot-jump.png');
+
     game.load.image('pipe', 'assets/pipe.png');
   },
 
@@ -17,6 +19,8 @@ var mainState = {
 
     this.robot.body.gravity.y = 1000;
 
+    this.robot.anchor.setTo(-0.2, 0.5);
+
     var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
     spaceKey.onDown.add(this.jump, this);
@@ -24,16 +28,32 @@ var mainState = {
     this.pipes = game.add.group();
 
     this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+
+    this.score = 0;
+    this.labelScore = game.add.text(20, 20, "0", {
+      font: "30px Arial", fill: "#ffffff"
+    });
   },
 
   update: function() {
     if (this.robot.y < 0 || this.robot.y > 490) {
       this.restartGame();
     }
+
+    if (this.robot.angle < 20) {
+      this.robot.angle += 1;
+    }
+
+    game.physics.arcade.overlap(this.robot, this.pipes, this.hitPipe, null, this);
   },
 
   jump: function() {
-    this.robot.body.velocity.y = -350;
+    if (this.robot.alive == false) {
+      return;
+    }
+    this.robot.body.velocity.y = -300;
+
+    game.add.tween(this.robot).to({angle: -20}, 100).start();
   },
 
   addOnePipe: function(x, y) {
@@ -57,6 +77,23 @@ var mainState = {
         this.addOnePipe(400, i * 60 + 10);
       }
     }
+
+    this.score += 1;
+    this.labelScore.text = this.score;
+  },
+
+  hitPipe: function() {
+    if (this.robot.alive == false) {
+      return;
+    }
+
+    this.robot.alive = false;
+
+    game.time.events.remove(this.timer);
+
+    this.pipes.forEach(function(p) {
+      p.body.velocity.x = 0;
+    }, this);
   },
 
   restartGame: function() {
@@ -64,7 +101,7 @@ var mainState = {
   }
 };
 
-var game = new Phaser.Game(400, 490);
+var game = new Phaser.Game(600, 490);
 
 game.state.add('main', mainState);
 
