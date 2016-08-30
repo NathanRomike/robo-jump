7c9bfa2cbaf6e08dafc559f123e94faf95a8c10a
NathanRomike <nathanromike@gmail.com>
2016-08-29 20:40:38 -0700

    Implement background.

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 var mainState = {
   preload: function() {
     game.load.image('robot', 'assets/robot.png');
-    game.load.image('robot', 'assets/robot-jump.png');
-    game.load.image('pipe', 'assets/pipe.png');
+    game.load.image('block', 'assets/block.png');
+    game.load.image('background', 'assets/stars.jpg');
   },
 
   create: function() {
-//    game.stage.backgroundColor = '#71c5cf';
+    game.add.tileSprite(0, 0, 1000, 600, 'background');
 
     game.physics.startSystem(Phaser.Physics.ARCADE);
     this.robot = game.add.sprite(100, 245, 'robot');
@@ -16,8 +16,8 @@ var mainState = {
     var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     spaceKey.onDown.add(this.jump, this);
     game.input.onDown.add(this.jump, this);
-    this.pipes = game.add.group();
-    this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+    this.blocks = game.add.group();
+    this.timer = game.time.events.loop(1500, this.addRowOfBlocks, this);
     this.score = 0;
     this.labelScore = game.add.text(20, 20, "0", {
       font: "30px Arial", fill: "#ffffff"
@@ -31,7 +31,7 @@ var mainState = {
     if (this.robot.angle < 20) {
       this.robot.angle += 1;
     }
-    game.physics.arcade.overlap(this.robot, this.pipes, this.restartGame, null, this);
+    game.physics.arcade.overlap(this.robot, this.blocks, this.restartGame, null, this);
   },
 
   jump: function() {
@@ -39,21 +39,21 @@ var mainState = {
     game.add.tween(this.robot).to({angle: -20}, 100).start();
   },
 
-  addOnePipe: function(x, y) {
-    var pipe = game.add.sprite(x, y, 'pipe');
-    this.pipes.add(pipe);
-    game.physics.arcade.enable(pipe);
-    pipe.body.velocity.x = -200;
-    pipe.checkWorldBounds = true;
-    pipe.outOfBoundsKill = true;
+  addOneBlock: function(x, y) {
+    var block = game.add.sprite(x, y, 'block');
+    this.blocks.add(block);
+    game.physics.arcade.enable(block);
+    block.body.velocity.x = -200;
+    block.checkWorldBounds = true;
+    block.outOfBoundsKill = true;
   },
 
-  addRowOfPipes: function() {
+  addRowOfBlocks: function() {
     var hole = Math.floor(Math.random() * 5) + 1;
 
     for (var i = 0; i < 8; i++) {
       if (i != hole && i != hole + 1) {
-        this.addOnePipe(700, i * 60 + 10);
+        this.addOneBlock(700, i * 60 + 10);
       }
     }
 
